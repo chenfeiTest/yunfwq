bef41892f32bb28002544ea79039ba9e9e0120a9
jayla25349 <jayla25349@gmail.com>
2018-07-17 17:41:08 +0800

    dev

--- src/main/java/com/redian/chat/controller/ChatController.java
+++ src/main/java/com/redian/chat/controller/ChatController.java
@@ -8,9 +8,7 @@ import com.redian.chat.domain.PageDTO;
 import com.redian.chat.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "chat")
@@ -19,15 +17,15 @@ public class ChatController {
     @Autowired
     private MessageService messageService;
 
-    @RequestMapping(name = "消息列表", value = "list")
+    @GetMapping(name = "消息列表")
     @ApiResponse
-    public Object list(@RequestBody MessageQuery query) {
+    public Object list(MessageQuery query) {
         Page<Message> messagePage = messageService.findAllByQuery(query);
         PageDTO<Message> pageDTO = new PageDTO<>(messagePage);
         return pageDTO;
     }
 
-    @RequestMapping(name = "发送消息", value = "send")
+    @PostMapping(name = "发送消息")
     @ApiResponse
     public Object send(@RequestBody MessageDTO dto) {
         messageService.sendMessage(dto);
