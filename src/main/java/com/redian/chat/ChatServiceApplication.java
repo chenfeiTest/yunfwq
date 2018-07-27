2251f009df666a6d4adb293be925254ccc40dbb6
jayla25349 <jayla25349@gmail.com>
2018-07-27 19:08:31 +0800

    IM聊天服务开发

--- src/main/java/com/redian/chat/ChatServiceApplication.java
+++ src/main/java/com/redian/chat/ChatServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -15,6 +16,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories
 @EnableJpaAuditing
 @EnableEurekaClient
+@EnableFeignClients
 public class ChatServiceApplication {
 
     public static void main(String[] args) {
