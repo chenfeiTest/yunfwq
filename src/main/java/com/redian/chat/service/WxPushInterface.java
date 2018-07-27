cb3d1eb83ff6cf78440019460073f48661013b5f
jayla25349 <jayla25349@gmail.com>
2018-07-27 21:17:49 +0800

    IM聊天服务开发

--- src/main/java/com/redian/chat/service/WxPushInterface.java
+++ src/main/java/com/redian/chat/service/WxPushInterface.java
@@ -4,11 +4,10 @@ import com.redian.chat.domain.PushDTO;
 import com.redian.errorcommon.DTO.Template;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient("base-service")
 public interface WxPushInterface {
 
     @PostMapping(value = "/wechat/push")
-    Template push(@RequestBody PushDTO pushDTO);
+    Template push(PushDTO pushDTO);
 }
