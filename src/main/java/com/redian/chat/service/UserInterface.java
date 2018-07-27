1255e247f92b5e6d699ff0ab203906935e14a925
jayla25349 <jayla25349@gmail.com>
2018-07-27 22:26:40 +0800

    IM聊天服务开发

--- src/main/java/com/redian/chat/service/UserInterface.java
+++ src/main/java/com/redian/chat/service/UserInterface.java
@@ -2,18 +2,19 @@ package com.redian.chat.service;
 
 import com.redian.errorcommon.DTO.Template;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient("user-service")
 public interface UserInterface {
 
-    @PostMapping(value = "/cusers/{openid}")
+    @GetMapping(value = "/cusers/{openid}")
     Template getCUser(@PathVariable String openid);
 
-    @PostMapping(value = "/busers/{openid}")
+    @GetMapping(value = "/busers/{openid}")
     Template getBUser(@PathVariable String openid);
 
-    @PostMapping(value = "/user/{openid}")
+    @GetMapping(value = "/user/{openid}")
     Template getShop(@PathVariable String openid);
 }
