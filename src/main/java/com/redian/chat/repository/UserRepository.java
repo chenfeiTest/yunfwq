4f784cffc92a6cb63566ab171d6164da5849e4be
jayla25349 <jayla25349@gmail.com>
2018-07-27 14:40:48 +0800

    IM聊天服务开发

--- src/main/java/com/redian/chat/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.redian.chat.repository;
-
-import com.redian.chat.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByOpenId(String openId);
-}
